3202fd3325dd25052c74d3e0c4da541d17906b74
lim <limjung99@naver.com>
2023-08-18 06:36:55 +0900

    불필요한 코드 삭제

--- BE/template.py
+++ BE/template.py
@@ -7,16 +7,3 @@ def generate_realistic():
     natural light, light background, photo, Studio lighting. \
     "
 
-## Not using
-'''
-def generate_anime():
-    return "Closeup face portrait of a {person}, \
-    smooth soft skin, big dreamy eyes,\
-    beautiful intricate colored hair, \
-    symmetrical, anime wide eyes, soft lighting, \
-    detailed face, by makoto shinkai, stanley artgerm lau,\
-    wlop, rossdraws, concept art, digital painting, looking into camera."
-
-def generate_cartoon():
-    return "anthro, very cute kid's film character, disney pixar zootopia character concept artwork, 3d concept, detailed fur, high detail iconic character for upcoming film, trending on artstation, character design, 3d artistic render, highly detailed, octane, blender, cartoon, shadows, lighting"
-'''
